cf3db693e7b91e7a1efffdcb47e4b6eb2caab84d
andrebisi <andregbisi@gmail.com>
2023-08-07 22:34:46 -0300

    inicio

---
+++ rotas/livro.js
@@ -0,0 +1,21 @@
+const {Router} = require("express")
+const {getLivros, getLivro, postLivro} = require("../controladores/livro")
+
+const router = Router()
+
+router.get('/', getLivros)
+
+router.get('/:id', getLivro)
+
+router.post('/', postLivro)
+
+router.patch('/', (req, res) => {
+    res.send('Voce fez uma requisição patch')
+})
+
+
+router.delete('/', (req, res) => {
+    res.send('Voce fez uma requisição delete')
+})
+
+module.exports = router
